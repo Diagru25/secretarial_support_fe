05e42fbfedd69ea6b91c15cfef71c52bc39f376f
diagru25 <diagru2510@gmail.com>
2023-03-02 08:52:13 +0700

    [fix] env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -14,7 +14,7 @@ module.exports = function (app) {
   app.use(
     "/upload",
     createProxyMiddleware({
-      target: "http://192.168.1.125:4444/api",
+      target: process.env.REACT_APP_BASE_URL,
       changeOrigin: true,
       pathRewrite: {
         "^/upload": "",
